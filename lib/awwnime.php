7dd3ce5776f782e98e6f05d107837845a525f281
Matt Hackmann <matt.hackmann@gmail.com>
2012-09-20 00:52:43 -0500

    reddit scanning functions added, findSimilarImages works off of new database model

--- lib/awwnime.php
+++ lib/awwnime.php
@@ -7,6 +7,10 @@ namespace Lib {
 	define('HISTOGRAM_BUCKETS', 4);
 	define('HISTORGAM_GRANULARITY', 256 / HISTOGRAM_BUCKETS);
 	
+	if (!defined('__INCLUDE__')) {
+		define('__INCLUDE__', './');
+	}
+	
 	class Awwnime {
 		
 		/**
@@ -65,6 +69,62 @@ namespace Lib {
 
 			return $retVal;
 
+		}
+		
+		/**
+		 * Gets a list of image URLs from an imgur album
+		 */
+		public static function getImgurAlbum($id) {
+			$data = self::curl_get_contents('http://api.imgur.com/2/album/' . $id . '.json');
+			$retVal = null;
+			if (strlen($data) > 0) {
+				$data = json_decode($data);
+				if (isset($data->album) && isset($data->album->images)) {
+					$retVal = array();
+					foreach ($data->album->images as $image) {
+						$retVal[] = $image->links->original;
+					}
+				}
+			}
+			return $retVal;
+		}
+		
+		/**
+		 * Processes an image from a post
+		 */
+		public static function processImage($id, $url) {
+			
+			$fileName = __INCLUDE__ . 'cache/' . $id;
+			if (self::downloadImage($url, $fileName)) {
+				
+				// Attach the correct file extension
+				$type = self::getImageType($fileName);
+				$ext = $type == 'image/jpeg' ? 'jpg' : ($type == 'image/gif' ? 'gif' : 'png');
+				rename($fileName, $fileName . '.' . $ext);
+				$fileName .= '.' . $ext;
+				
+				// Histogram and dump to the database
+				$hist = self::generateHistogram($fileName);
+				$params = array( ':id' => $id, ':url' => $url );
+				$cols = '';
+				$vals = '';
+				for ($i = 0; $i < HISTOGRAM_BUCKETS; $i++) {
+					$params[':red' . $i] = $hist->red[$i];
+					$params[':green' . $i] = $hist->green[$i];
+					$params[':blue' . $i] = $hist->blue[$i];
+					$cols .= ', image_hist_r' . ($i + 1);
+					$cols .= ', image_hist_g' . ($i + 1);
+					$cols .= ', image_hist_b' . ($i + 1);
+					$vals .= ', :red' . $i;
+					$vals .= ', :green' . $i;
+					$vals .= ', :blue' . $i;
+				}
+				
+				$query = 'INSERT INTO images (post_id, image_url' . $cols . ') VALUES (:id, :url' . $vals . ')';
+				Db::Query($query, $params);
+				
+			}
+			
 		}
 		
 		/**
@@ -134,11 +194,11 @@ namespace Lib {
 					$params[':red' . $i] = $histogram->red[$i - 1];
 					$params[':green' . $i] = $histogram->green[$i - 1];
 					$params[':blue' . $i] = $histogram->blue[$i - 1];
-					$query .= 'ABS(post_hist_r' . $i . ' - :red' . $i . ') + ABS(post_hist_g' . $i . ' - :green' . $i . ') + ABS(post_hist_b' . $i . ' - :blue' . $i . ') + ';
+					$query .= 'ABS(i.image_hist_r' . $i . ' - :red' . $i . ') + ABS(i.image_hist_g' . $i . ' - :green' . $i . ') + ABS(i.image_hist_b' . $i . ' - :blue' . $i . ') + ';
 				}
 				
 				// Find the top five most similar images in the database
-				$result = Db::Query('SELECT post_id, post_title, post_link, post_date, ' . $query . '0 AS distance FROM posts WHERE post_hist_r1 IS NOT NULL ORDER BY distance LIMIT ' . $limit, $params);
+				$result = Db::Query('SELECT i.post_id, p.post_title, i.image_url, p.post_date, ' . $query . '0 AS distance FROM images i INNER JOIN posts p ON p.post_id = i.post_id ORDER BY distance LIMIT ' . $limit, $params);
 				if ($result) {
 				
 					$retVal = array();
@@ -146,7 +206,7 @@ namespace Lib {
 						$obj = new stdClass;
 						$obj->id = $row->post_id;
 						$obj->title = $row->post_title;
-						$obj->url = self::_parseUrl($row->post_link);
+						$obj->url = self::_parseUrl($row->image_url);
 						$obj->similarity = abs(100 - (100 * ($row->distance / 12)));
 						$obj->date = $row->post_date;
 						$retVal[] = $obj;
@@ -158,6 +218,60 @@ namespace Lib {
 			
 			return $retVal;
 		
+		}
+		
+		/**
+		 * Gets a page listing from the awwnime subreddit
+		 */
+		public static function getAwwnimePage($pageNum = 0, $afterId = null) {
+			
+			$pageNum *= 25;
+			$retVal = self::curl_get_contents('http://www.reddit.com/r/awwnime/.json?count=' . $pageNum . '&after=' . $afterId);
+			if (strlen($retVal) > 0) {
+				$retVal = json_decode($retVal);
+				if (isset($retVal->data)) {
+					$retVal = $retVal->data;
+				}
+			}
+			
+			return $retVal;
+			
+		}
+		
+		/**
+		 * Generates keywords from the provided text
+		 */
+		public static function getKeywords($text) {
+			$stop = '/\b(a|able|about|above|abroad|according|accordingly|across|actually|adj|after|afterwards|again|against|ago|ahead|ain\'t|all|allow|allows|almost|alone|along|alongside|already|also|although|always|am|amid|amidst|among|amongst|an|and|another|any|anybody|anyhow|anyone|anything|anyway|anyways|anywhere|apart|appear|appreciate|appropriate|are|aren\'t|around|as|a\'s|aside|ask|asking|associated|at|available|away|awfully|back|backward|backwards|be|became|because|become|becomes|becoming|been|before|beforehand|begin|behind|being|believe|below|beside|besides|best|better|between|beyond|both|brief|but|by|came|can|cannot|cant|can\'t|caption|cause|causes|certain|certainly|changes|clearly|c\'mon|co|co.|com|come|comes|concerning|consequently|consider|considering|contain|containing|contains|corresponding|could|couldn\'t|course|c\'s|currently|dare|daren\'t|definitely|described|despite|did|didn\'t|different|directly|do|does|doesn\'t|doing|done|don\'t|down|downwards|during|each|edu|eg|eight|eighty|either|else|elsewhere|end|ending|enough|entirely|especially|et|etc|even|ever|evermore|every|everybody|everyone|everything|everywhere|ex|exactly|example|except|fairly|far|farther|few|fewer|fifth|first|five|followed|following|follows|for|forever|former|formerly|forth|forward|found|four|from|further|furthermore|get|gets|getting|given|gives|go|goes|going|gone|got|gotten|greetings|had|hadn\'t|half|happens|hardly|has|hasn\'t|have|haven\'t|having|he|he\'d|he\'ll|hello|help|hence|her|here|hereafter|hereby|herein|here\'s|hereupon|hers|herself|he\'s|hi|him|himself|his|hither|hopefully|how|howbeit|however|hundred|i\'d|ie|if|ignored|i\'ll|i\'m|immediate|in|inasmuch|inc|inc.|indeed|indicate|indicated|indicates|inner|inside|insofar|instead|into|inward|is|isn\'t|it|it\'d|it\'ll|its|it\'s|itself|i\'ve|just|keep|keeps|kept|know|known|knows|last|lately|later|latter|latterly|least|less|lest|let|let\'s|like|liked|likely|likewise|little|look|looking|looks|low|lower|ltd|made|mainly|make|makes|many|may|maybe|mayn\'t|me|mean|meantime|meanwhile|merely|might|mightn\'t|mine|minus|miss|more|moreover|most|mostly|mr|mrs|much|must|mustn\'t|my|myself|name|namely|nd|near|nearly|necessary|need|needn\'t|needs|neither|never|neverf|neverless|nevertheless|new|next|nine|ninety|nobody|non|none|nonetheless|noone|no-one|nor|normally|not|nothing|notwithstanding|novel|now|nowhere|obviously|of|off|often|oh|ok|okay|old|once|one|ones|one\'s|only|onto|opposite|or|other|others|otherwise|ought|oughtn\'t|our|ours|ourselves|out|outside|over|overall|own|particular|particularly|past|per|perhaps|placed|please|plus|possible|presumably|probably|provided|provides|que|quite|qv|rather|rd|re|really|reasonably|recent|recently|regarding|regardless|regards|relatively|respectively|right|round|said|same|saw|say|saying|says|second|secondly|see|seeing|seem|seemed|seeming|seems|seen|self|selves|sensible|sent|serious|seriously|seven|several|shall|shan\'t|she|she\'d|she\'ll|she\'s|should|shouldn\'t|since|six|some|somebody|someday|somehow|someone|something|sometime|sometimes|somewhat|somewhere|soon|sorry|specified|specify|specifying|still|sub|such|sup|sure|take|taken|taking|tell|tends|th|than|thank|thanks|thanx|that|that\'ll|thats|that\'s|that\'ve|the|their|theirs|them|themselves|then|thence|there|thereafter|thereby|there\'d|therefore|therein|there\'ll|there\'re|theres|there\'s|thereupon|there\'ve|these|they|they\'d|they\'ll|they\'re|they\'ve|thing|things|think|third|thirty|this|thorough|thoroughly|those|though|three|through|throughout|thru|thus|till|together|too|took|toward|towards|tried|tries|truly|try|trying|t\'s|twice|two|un|under|underneath|undoing|unfortunately|unless|unlike|unlikely|until|unto|up|upon|upwards|us|use|used|useful|uses|using|usually|v|value|various|versus|very|via|viz|vs|want|wants|was|wasn\'t|way|we|we\'d|welcome|well|we\'ll|went|were|we\'re|weren\'t|we\'ve|what|whatever|what\'ll|what\'s|what\'ve|when|whence|whenever|where|whereafter|whereas|whereby|wherein|where\'s|whereupon|wherever|whether|which|whichever|while|whilst|whither|who|who\'d|whoever|whole|who\'ll|whom|whomever|who\'s|whose|why|will|willing|wish|with|within|without|wonder|won\'t|would|wouldn\'t|yes|yet|you|you\'d|you\'ll|your|you\'re|yours|yourself|yourselves|s|you\'ve|zero)\b/i';
+			$retVal = strtolower($text);
+			
+			// Remove special characters, punctuation, and stop words
+			$retVal = htmlspecialchars_decode($retVal);
+			$retVal = str_replace(array ('.', '>', '<', '\'', '|', '[', ']', '(', ')', '{', '}', '!', '@', '#', '$', '%', '^', '&', '*', '?', '"', ':', ',', '_'), ' ', $retVal);
+			$retVal = str_replace ('  ', ' ', $retVal);
+			$retVal = preg_replace($stop, '', $retVal);
+			
+			// Remove duplicate words, clean up spaces
+			$temp = explode(' ', $retVal);
+			$retVal = array();
+			for ($i = 0, $count = count($temp); $i < $count; $i++) {
+				$temp[$i] = trim($temp[$i]);
+				if (strlen($temp[$i]) > 1) {
+					$place = true;
+					for ($j = 0, $c = count($retVal); $j < $c; $j++) {
+						if ($retVal[$j] == $temp[$i]) {
+							$place = false;
+							break;
+						}
+					}
+					if ($place) {
+						$retVal[] = $temp[$i];
+					}
+				}
+			}
+			
+			return implode(' ', $retVal);
+			
 		}
 		
 		/**
@@ -221,11 +335,12 @@ namespace Lib {
 		 * @param string $url Url to retrieve
 		 * @return string Data received
 		 */
-		function curl_get_contents($url) {
+		private static function curl_get_contents($url) {
 			$c = curl_init($url);
 			curl_setopt($c, CURLOPT_USERAGENT, 'moe downloader by /u/dxprog');
 			curl_setopt($c, CURLOPT_FOLLOWLOCATION, true);
 			curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($c, CURLOPT_TIMEOUT, 5);
 			return curl_exec($c);
 		}
 	
